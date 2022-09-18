7147154d656e729dd8d99e94a072d19b38096675
Miyazawa Akira <pecorarista@gmail.com>
2022-09-18 18:18:16 +0900

    Update css-related packages

--- tasks/style.mjs
+++ tasks/style.mjs
@@ -3,15 +3,11 @@ import gulp from 'gulp';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
-import sassLint from 'gulp-sass-lint';
 
 import { source, dest } from './config.mjs';
 
 export const compileSass = (done) => {
   gulp.src(source.sassFiles)
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
     .pipe(sass())
     .pipe(gulp.dest(dest.styleFiles));
   done();
