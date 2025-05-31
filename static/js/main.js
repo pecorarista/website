b0a9b6f7b90ca8638574b70fce8d852e58157815
pecorarista <pecorarista@users.noreply.github.com>
2025-05-31 10:18:19 +0000

    deploy: ba4e3b2490777fafd222dd376328edc624e722c5

--- static/js/main.js
+++ static/js/main.js
@@ -1,77 +1,8 @@
-function transform(element) {
-
-  const ELEMENT_NODE = 1;
-
-  const hiragana = '\u3040-\u309F～';
-  const katakana = '\u30A0-\u30FF';
-  const kanji = '\u2E80-\u2FFF\u31C0-\u31EF\u3300-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\uFE30-\uFE4F';
-
-  const punct = '[—@&=_\,\.\?\!\$\%\^\*\-\+\/]';
-  const left = `[\\[({'"<«‘“⟨]`;
-  const right = `[\\])}'">»’”⟩]`;
-
-  const cjk = '[' +  [hiragana, katakana, kanji].join('') + ']';
-  const latin = '[A-Za-z0-9\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]' + '|' + punct;
-  const greek = '[\u0391-\u03C0]';
-
-  const patterns = {
-    quarter: [
-      '(' + cjk + ')(' + latin + '|' + greek + '|' + left + ')',
-      '(' + latin + '|' + greek + '|' + right + ')(' + cjk + ')',
-      '(・)(' + cjk + ')',
-      '(' + latin + '|' + greek + '|' + cjk + ')([・])',
-      '([：])([『])',
-      '(' + cjk + ')([：])'
-    ],
-    // if proportional
-    half: [
-      '([、。，．）」』])(' + left + '|' + latin + '|' + cjk + ')',
-      '(' + latin + '|' + greek + '|' + cjk + ')([（「『])'
-    ]
-  };
-
-  Object.keys(patterns).forEach(space => {
-    patterns[space].forEach(pattern => {
-      findAndReplaceDOMText(element, {
-        find: new RegExp(pattern, 'ig'),
-        replace: '$1<' + `${space}` + '>$2'
-      })
-    });
-
-    findAndReplaceDOMText(element, {
-      find: `<${space}>`,
-      replace: () => {
-        let newElement = document.createElement('span');
-        newElement.className = space;
-        return newElement;
-      }
-    });
-
-    // Required to keep space before <code></code>
-    document.querySelectorAll(`* > span.${space}:first-child`).forEach(child => {
-      let p = child.parentNode;
-      if (p.firstChild.nodeType == ELEMENT_NODE) {
-        let newElement = document.createElement('span');
-        newElement.className = space;
-        p.parentNode.insertBefore(newElement, p);
-        p.querySelector(`span.${space}:first-child`).remove();
-      }
-    });
-
-    element.normalize();
-
-  });
-}
-
-
 document.addEventListener('DOMContentLoaded', event => {
 
   document.querySelectorAll('a[href^="http"]').forEach(element => {
     element.setAttribute('target', '_blank');
   });
 
-  document.body.querySelectorAll('p, li, h1, h2, h3, h4, h5, figcaption').forEach(element => {
-    transform(element);
-  });
 
 });
